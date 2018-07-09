1db938b8de8bc9dce5ac82f7a1902992ec754fcb
bosung <bosung.kim3416@gmail.com>
2018-07-09 23:53:05 +0900

    add preprocess for vocab

--- preprocess.py
+++ preprocess.py
@@ -1,18 +1,67 @@
 
+class Vocabulary:
+
+    def __init__(self):
+        self.word2idx = {}
+        self.idx2word = {}
+        self.idx = 0
+        print("New Vocabulary!")
+
+    def load(self, vocab_path):
+        """ load vocabulary dictionary """
+        self.idx = 0
+        with open(vocab_path, "r") as f:
+            for line in f.readlines():
+                word = line.strip()
+                self.word2idx[word] = self.idx
+                self.idx2word.append(word)
+                self.idx += 1
+        f.close()
+        print("[DONE] load vocabulary from %s" % vocab_path)
+
+    def build(self, train_path, vocab_size=16000):
+        """ build vocabulary from train data """
+        word_count = {}
+        with open(train_path, "r") as f:
+            for line in f.readlines():
+                words = [x.strip() for x in line.split(" ")]
+                for w in words:
+                    if w not in word_count:
+                        word_count[w] = 1
+                    else:
+                        word_count[w] += 1
+        f.close()
+        print("[INFO] total %s vocabulary" % str(len(word_count)))
+        print("[INFO] build %s vocabulary dict from %s" % (str(vocab_size), train_path))
+        vocab_idx = 0
+        for key, value in reversed(sorted(word_count.items(), key=lambda i: (i[1], i[0]))):
+            self.word2idx[key] = vocab_idx
+            self.idx2word[vocab_idx] = key
+            vocab_idx += 1
+            if vocab_idx == vocab_size:
+                break
+        print("[DONE] build success!")
+
+    def sentence2vector(self, sentence):
+        # OOV -> <unk>
+        # TODO lowercass??
+        vector = []
+        for w in sentence.split(" "):
+            if w in self.word2idx:
+                vector.append(self.word2idx[w])
+            else:
+                vector.append(-1)
+        return vector
+
+
 def build_vocab(path):
-
-    word_to_idx, idx_to_word = {}, []
-    index = 0
-
-    f = open(path, "r")
-
-    for line in f.readlines():
-        word = line.strip()
-        word_to_idx[word] = index
-        idx_to_word.append(word)
-        index += 1
+    vocab = Vocabulary()
+    vocab.build(path)
+    return vocab
 
 
-def sentence_to_vector(sentence):
-    idxs = [word_to_idx[w] for x in sentence.split(" ")]
+vocab = Vocabulary()
+vocab.build("data/corpus.tc.en")
+result = vocab.sentence2vector("Obama relation new automatic automa")
+print(result)
 
